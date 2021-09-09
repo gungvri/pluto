7d27a3670a29c6a4ec58a9aaf6aa8eb04131c67f
Gábor <gabor.ungvri@gmail.com>
2021-09-09 07:57:53 +0200

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,11 @@
 
 @section('content')
 
-<p>Szia!</p>
+<div class="card">
+    <div class="card-content">
+        <span class="card-title">Hellóbelló</span>
+        <p>Az ígéret szép szó, de a Landwirtschaftsausstellung még szebb.</p>
+    </div>
+</div>
 
 @endsection
